2f21087933a190f1483a93b372f5047491f2783b
Michele Fenech Adami <michele.fenechadami@netrefer.com>
2020-02-17 19:56:12 +0100

    Added ExportToPDF

---
+++ src/ExportToPDF.js
@@ -0,0 +1,170 @@
+//var conf = require('./config.js');
+PDFDocument = require('pdfkit');
+fs = require('fs');
+
+/**
+ * Add new lines when lines are too long
+ */
+function polishString(str) {
+    var maxLength = 30;
+    var arr = str.split(" ");
+    var newString = "";
+    var chars = 0;
+
+    arr.forEach(function(word) {
+        chars = chars + word.length;
+        if (chars >= maxLength) {
+            newString += word + "\n";
+            chars = 0;
+        } else {
+            newString += word + " ";
+        }
+    });
+    return newString;
+}
+function createDirectory(dir){
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+}
+
+function RemoveDirectory(path){
+  var fileStream = require('fs-extra');
+  fileStream.remove(path);
+
+}
+
+function createQR(url,filename) {
+  console.log(filename);
+  var qr = require('qr-image');
+
+  var qr_svg = qr.image(url, { type: 'png' });
+  fs.writeFileSync('temp/' + filename + '.png', qr.imageSync(url, { type: 'png' }));
+}
+
+function createPDFDocument() {
+    //Create a document
+    doc = new PDFDocument({margin: 0});
+
+
+    // Pipe its output somewhere, like to a file or HTTP response
+    // See below for browser usage
+    var stream = fs.createWriteStream(process.env.pdf_filename);
+    doc.pipe(stream);
+
+    stream.on('error', function(err) {
+        console.log('error writing to PDF ');
+    });
+
+    stream.on('end', function() {
+        cb();
+    });
+}
+
+module.exports = {
+    writetoPDF: function(AddedToSprint) {
+            createPDFDocument();
+            var y = 0;
+            var j = 0;
+
+            var tickets = ArrayToExport.slice();
+            createDirectory('./temp');
+            while (tickets.length) {
+
+
+            //     //map used for shortening project names
+                var ProjectAliases = {
+                    ZettaBox_Web_NextGen: "nxtgn",
+                    ZettaBox_Web: "Web",
+                    ZettaBox_WinClient: "Win",
+                    ZettaBox_Services: "SRV"
+                };
+                var re = new RegExp(Object.keys(ProjectAliases).join("|"), "gi");
+
+
+                var page = tickets.splice(0, 6);
+                var ticketNo = 0;
+                page.forEach(function(ticket) {
+
+
+                    isLeft = ticketNo % 2;
+
+                    if (isLeft === 0) {
+                        //is left
+                        x = 0;
+                    } else {
+                        x = 360;
+                    }
+
+                    doc.rect(x, y * 260, 250, 250)
+                        .lineWidth(1)
+                        .fillOpacity(1000.1);
+
+
+                    //colour the ticket according to type of ticket
+                    if (page[ticketNo][process.env.printed_tickets_type] == "Bug") {
+                        doc.fillAndStroke("#ffe6e6", "#200");
+                    }
+                    if (page[ticketNo][process.env.printed_tickets_type] == "Technical") {
+                        doc.fillAndStroke("#fff0b3", "#200");
+                    }
+                    if (page[ticketNo][process.env.printed_tickets_type] == "DevOps Sub task") {
+                        doc.fillAndStroke("#ccffcc", "#200");
+                    } else {
+                        doc.fillAndStroke("white", "#200");
+                    }
+
+                    //display Ticket number
+                    doc.font('./Assets/Fonts/Times_New_Roman_Bold.ttf')
+                        .fontSize(50)
+                        .fillColor('black')
+                        .text(page[ticketNo][process.env.printed_tickets_center_bold].replace(re, function(matched) {
+                            return ProjectAliases[matched];
+                        }), (x + 5), (y * 260) + 80);
+
+
+                    //display Header of ticket
+                    doc.fontSize(13);
+                    doc.font('./Assets/Fonts/Times_New_Roman_Normal.ttf');
+                    doc.text(polishString(page[ticketNo][process.env.printed_tickets_small_text]));
+
+                    //display story points
+                    // doc.font('./assets/fonts/Times_New_Roman_Normal.ttf')
+                    //     .fontSize(30)
+                    //     .fillColor('black')
+                    //     .text(page[ticketNo][process.env.printed_tickets_topRight], x + 200, (y * 260) + 10);
+
+
+                    //display Asignee first name
+                    var FullName = page[ticketNo][process.env.printed_tickets_topLeft].split(" ");
+                    doc.font('./Assets/Fonts/Times_New_Roman_Normal.ttf')
+                        .fontSize(20)
+                        .fillColor('black')
+                        .text(FullName[0], x + 10, (y * 260) + 10);
+
+                    //write QR image
+                    // createQR(conf.YouTrack_Search_URL + page[ticketNo][conf.ticketID], page[ticketNo][conf.ticketID]);
+                    // doc.image('temp/' + page[ticketNo][process.env.ticketID] + '.PNG', x + 100, (y * 260) + 198 , {width:50});
+
+                    
+                    ticketNo++;
+                    if (isLeft !== 0) {
+                        y++;
+                    }
+                });
+                y = 0;
+                if (tickets.length ) {
+                  doc.addPage(
+                    {margin: 0})
+                }
+            }
+
+
+
+            doc.save();
+            //Finalize PDF file
+            doc.end();
+            RemoveDirectory('./temp');
+
+        }
+};
